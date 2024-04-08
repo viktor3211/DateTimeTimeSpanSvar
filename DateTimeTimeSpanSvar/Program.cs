704612eebb6fe199fbe412bb636cbeef9e89582e
Viktor Reuterhäll <viktor.reuterhall@skola.sala.se>
2024-04-08 10:53:08 +0200

    Uppgift 2, 3, 4, 5, 6

--- DateTimeTimeSpanSvar/Program.cs
+++ DateTimeTimeSpanSvar/Program.cs
@@ -8,8 +8,11 @@
                 Console.WriteLine("DateTime & TimeSpan: Svar till uppgifter:");
                 Console.WriteLine("Uppgift 1: " + Uppgift1());
                 Console.WriteLine("Uppgift 2: " + Uppgift2());
-                // TODO uppg 3, 4 osv.
-            }
+                Console.WriteLine("Uppgift 3: " + Uppgift3());
+                Console.WriteLine("Uppgift 4: " + Uppgift4());
+                Console.WriteLine("Uppgift 5: " + Uppgift5());
+                Console.WriteLine("Uppgift 6: " + Uppgift6());
+        }
 
             public static string Uppgift1()
             {
@@ -21,10 +24,42 @@
 
             public static string Uppgift2()
             {
-                return "123"; // TODO
+            DateTime dt1 = new DateTime(2006, 05, 06);
+            DateTime dt2 = DateTime.Now;
+            TimeSpan ts = dt2 - dt1;
+            return ts.ToString();
             }
+            public static string Uppgift3()
+            {
+            DateTime dt1 = new DateTime(2006, 05, 06);
+            DateTime dt2 = DateTime.Now;
+            TimeSpan ts = (dt2 - dt1) * 2;
+            return ts.ToString();
+            }
+            public static string Uppgift4()
+            {
+            DateTime dt1 = new DateTime(2022, 03, 26, 12, 00, 00);
+            DateTime dt2 = new DateTime(2022, 03, 27, 12, 00, 00);
+            TimeSpan ts = (dt2 - dt1);
+            return ts.TotalHours.ToString();
+            }
+            public static string Uppgift5()
+            {
+            DateTime dt1 = new DateTime(2023, 01, 01);
+            DateTime dt2 = new DateTime(2024, 01, 01);
+            TimeSpan ts = (dt2 - dt1);
 
-
-        
+            DateTime dt3 = new DateTime(2024, 01, 01);
+            DateTime dt4 = new DateTime(2025, 01, 01);
+            TimeSpan ts2 = (dt4 - dt3);
+            return ts.ToString() + " " + ts2.ToString();
+            }
+            public static string Uppgift6()
+            {
+            DateTime dt1 = new DateTime(2015, 07, 01, 01, 59, 30);
+            DateTime dt2 = new DateTime(2015, 07, 01, 02, 00, 30);
+            TimeSpan ts = (dt2 - dt1);
+            return ts.TotalSeconds.ToString();
+            }
     }
 }
