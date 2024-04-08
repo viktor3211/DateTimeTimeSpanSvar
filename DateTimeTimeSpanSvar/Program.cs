c8a858845e6ccd7478c0888a0f01f443b4673558
Viktor Reuterhäll <viktor.reuterhall@skola.sala.se>
2024-04-08 10:10:56 +0200

    Add project files.

---
+++ DateTimeTimeSpanSvar/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DateTimeTimeSpanSvar
+{
+    internal class Program
+    {
+        
+            public static void Main()
+            {
+                Console.WriteLine("DateTime & TimeSpan: Svar till uppgifter:");
+                Console.WriteLine("Uppgift 1: " + Uppgift1());
+                Console.WriteLine("Uppgift 2: " + Uppgift2());
+                // TODO uppg 3, 4 osv.
+            }
+
+            public static string Uppgift1()
+            {
+            DateTime dt1 = new DateTime(2009, 12, 18);
+            DateTime dt2 = new DateTime(2022, 12, 14);
+            TimeSpan ts = dt2 - dt1;
+            return ts.ToString();
+            }
+
+            public static string Uppgift2()
+            {
+                return "123"; // TODO
+            }
+
+
+        
+    }
+}
